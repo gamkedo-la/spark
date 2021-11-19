3b1136e1a5bc83c64e9c58588e049985a99ae529
Christer Kaitila <breakdance@mcfunkypants.com>
2021-11-19 10:03:13 -0800

    added particles to the titlescreen

--- js/titleState.js
+++ js/titleState.js
@@ -10,6 +10,11 @@ import { PlayState }        from "./playState.js";
 import { MenuState }        from "./menuState.js";
 import { Base } from "./base/base.js";
 
+// experimenting with adding pizazz to the title screen:
+import { SparkFx }          from "./sparkFx.js";
+import { Generator }        from "./base/generator.js";
+import { TtlCondition }     from "./base/particles.js";
+
 class TitleState extends State {
     cpre(spec) {
         super.cpre(spec);
@@ -19,6 +24,9 @@ class TitleState extends State {
             //resize: true,
             tag: "cvs.0",
             xchildren: [
+                // TODO: can we somehow spawn a particle here? or animate in any way?
+                // can't figure out how to create new sparkFx() etc
+                // how do we access each frame on update()? hmmm
                 Templates.panel("titleBG", { xsketch: {cls: "Media", tag: "menuBackground"}, xxform: { top: 0, bottom: 0, left: 0, right: 0}}),
                 Templates.titleDark(null, "Spark", { xxform: { left: .301, right: .301, top: .305, bottom: .545}}),
                 Templates.titleText(null, "Spark", { xxform: { left: .3, right: .3, top: .3, bottom: .55}}),
@@ -34,7 +42,9 @@ class TitleState extends State {
         super.cpost(spec);
         Util.bind(this, "onKeyDown", "onClicked");
         Keys.evtKeyPressed.listen(this.onKeyDown);
-        Mouse.evtClicked.listen(this.onClicked)
+        Mouse.evtClicked.listen(this.onClicked);
+        // add some particles to the title screen that follow the mouse
+        Mouse.evtMoved.listen(this.onMoved);
     }
 
     onKeyDown(evt) {
@@ -52,9 +62,40 @@ class TitleState extends State {
         Base.instance.stateMgr.swap(state);
     }
 
+    // sparks follow the mouse
+    onMoved(evt) {
+        //console.log("title screen mouse moved!");
+        //console.log(evt);
+        
+        // FIXME: these particle emitters NEVER DIE?!?
+        let xfx = {
+            cls: "SparkFx",
+            getx: () => evt.x,
+            gety: () => evt.y,
+            xxform: {scalex:1, scaley:1}, 
+            //count: 1, // no effect
+            //lifespan: 0.1, // no effect
+            // how to set lifespan?
+            conditions: { ttl: new TtlCondition({ttl: 10})}, // no effect
+            // how to trigger emitter.done? how to move a single emitter?
+            depth: 100
+        };
+        
+        // only create one emitter
+        // if (!this.mouseFX) this.mouseFX = Generator.generate(xfx);
+        // and move it around. FIXME no effect
+        // this.mouseFX.x = evt.x;
+        // this.mouseFX.y = evt.x;
+        
+        // very spammy - they NEVER DIE
+        if (Math.random()<0.3) this.mouseFX = Generator.generate(xfx);
+        
+    }
+
     destroy() {
         Keys.evtKeyPressed.ignore(this.onKeyDown);
         Mouse.evtClicked.ignore(this.onClicked);
+        Mouse.evtMoved.ignore(this.onMoved);
         super.destroy();
     }
 
